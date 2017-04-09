21da1cc15eea25502678ccdff104f6607a30b552
soojikim <soojikim5@gmail.com>
2017-04-09 00:15:05 -0400

    Initial Commit

---
+++ Fitclip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fitclip
+//
+//  Created by Sooji Kim on 4/9/17.
+//  Copyright © 2017 Sooji & Ulysses. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
